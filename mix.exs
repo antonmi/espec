570f74f6b215cafd615ddb7b9caab9218ab2d2fc
Anton Mishchuk <anton.mishchuk@onapp.com>
2015-03-25 10:26:37 +0200

    prepare hex

--- mix.exs
+++ mix.exs
@@ -3,28 +3,29 @@ defmodule ESpec.Mixfile do
 
   def project do
     [app: :espec,
-     version: "0.0.1",
-     elixir: "~> 1.1-dev",
+     version: "0.1.0",
+     elixir: "~> 1.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
+
+  defp description do
+    "ESpec is RSpec for Elixir"
+  end
+
+  defp package do
+   [
+     files: ["lib", "spec"]
+     contributors: ["Anton Mishchuk"],
+     licenses: ["MIT"],
+     links: %{"github" => "https://github.com/antonmi/espec"}
+   ]
+ end
 end
