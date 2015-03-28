48fd8a3a722a3ee05acf4c5dbc27c42dab13a6e0
Anton Mishchuk <anton.mishchuk@gmail.com>
2015-03-28 23:03:50 +0200

    test if travis fails

--- spec/assertions/be_between_spec.exs
+++ spec/assertions/be_between_spec.exs
@@ -9,7 +9,7 @@ defmodule ESpec.Assertions.BetweenSpec do
       it do: expect(1+1).to_not be_between(3, 5)
     end
 
-    xcontext "Errors" do
+    context "Errors" do
       it do: expect(1+1).to be_between(3, 5)
       it do: expect(1+1).to_not be_between(1,3)
     end
