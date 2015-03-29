f371141e79a23b7fb5dcf6059ab1ecc098266adf
Anton Mishchuk <anton.mishchuk@gmail.com>
2015-03-30 01:09:09 +0300

    some_spec

--- spec/some_spec.exs
+++ spec/some_spec.exs
@@ -1,24 +1,12 @@
 defmodule SomeSpec do
-  
   use ESpec
 
-  def add(value), do: Agent.update(:some_spec_agent, &(Set.put(&1, :a)))
-  def count, do: length(Agent.get(:some_spec_agent, &(&1)) |> Set.to_list)
+  subject(1+1)
+  it do: is_expected.to eq(2)
 
-  before do: Agent.start_link(fn -> HashSet.new end, name: :some_spec_agent) 
-  finally do: Agent.stop(:some_spec_agent)
-
-  it "change to" do
-    f1 = fn -> SomeSpec.add(:value) end
-    f2 = &SomeSpec.count/0
-    expect(f1).to change(f2, 1)    
+  context "with block" do
+    subject do: 2+2
+    it do: is_expected.to eq(4)
+    it do: expect(true).to be true
   end
-
-  it "change from to" do
-    f1 = fn -> SomeSpec.add(:value) end
-    f2 = &SomeSpec.count/0
-    expect(f1).to change(f2, 0, 1)    
-  end
-
-
 end 
