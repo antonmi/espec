c1a2d966fdfe88638efc8927ab58d34328da3349
Anton Mishchuk <anton.mishchuk@onapp.com>
2015-06-03 16:13:49 +0300

    fix warnings

--- lib/espec/assertions/dict/have_value.ex
+++ lib/espec/assertions/dict/have_value.ex
@@ -12,7 +12,7 @@ defmodule ESpec.Assertions.Dict.HaveValue do
     "`#{inspect dict}` #{to} value `#{inspect val}`."
   end 
 
-  defp error_message(dict, val, result, positive) do
+  defp error_message(dict, val, _result, positive) do
     to = if positive, do: "to", else: "to not"
     but = if positive, do: "has not", else: "has"
     "Expected `#{inspect dict}` #{to} have value `#{inspect val}` but it #{but}."
--- lib/espec/assertions/string/be_valid_string.ex
+++ lib/espec/assertions/string/be_valid_string.ex
@@ -2,17 +2,17 @@ defmodule ESpec.Assertions.String.BeValidString do
 
   use ESpec.Assertions.Interface
 
-  defp match(string, val) do
+  defp match(string, _val) do
     result = String.valid?(string)
     {result, result}
   end
 
-  defp success_message(string, val, _result, positive) do
+  defp success_message(string, _val, _result, positive) do
     to = if positive, do: "is", else: "is not"
     "`#{inspect string}` #{to} valid."
   end
   
-  defp error_message(string, val, result, positive) do
+  defp error_message(string, _val, _result, positive) do
     to = if positive, do: "to", else: "to not"
     but = if positive, do: "it isn't", else: "it is"
     "Expected `#{inspect string}` #{to} be valid but #{but}."
