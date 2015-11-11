413b807c02864396f8dc52f67a1b217727393f8c
Lei Yang <leiyangyou@gmail.com>
2015-11-12 01:05:31 +0800

    assertions/enum_string/have: fixed broken success_message

--- spec/assertions/enum/have_spec.exs
+++ spec/assertions/enum/have_spec.exs
@@ -2,14 +2,17 @@ defmodule ESpec.Assertions.Enum.HaveSpec do
   use ESpec, async: true
 
   let :range, do: (1..3)
+  let :list, do: [{}]
   
   context "Success" do
     it do: expect(range).to have(2)
     it do: expect(range).to_not have(4)
+    it do: expect(list).to have({})
   end
 
   xcontext "Error" do
     it do: expect(range).to_not have(2)
     it do: expect(range).to have(4)
+    it do: expect(list).to_not have({})
   end
 end
