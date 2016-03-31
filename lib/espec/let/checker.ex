b223395726edf8111478f1a26c3e0c6b340a50b9
Anton Mishchuk <anton.mishchuk@gmail.com>
2016-04-01 00:38:08 +0300

    Move check functionality to Checker

--- lib/espec/let/checker.ex
+++ lib/espec/let/checker.ex
@@ -1,27 +1,35 @@
 defmodule ESpec.Let.Checker do
   def check(context, defined_lets, escaped_block) do
-    context_lets = context
-    |> ESpec.Example.extract(ESpec.Let)
-    |> Enum.map(&(&1.var))
-
-    funcs = ESpec.Let.AstParser.function_list(escaped_block)
+    context_lets = context_lets(context)
 
     diff = Enum.uniq(defined_lets) -- context_lets
     |> Enum.map(&("#{&1}/0"))
 
-    if Enum.member?(funcs, "should/1") || Enum.member?(funcs, "is_expected/0") do
-      funcs = ["subject/0" | funcs]
+    functions = all_functions(escaped_block)
+    leaking_func = Enum.find(diff, &(Enum.member?(functions, &1)))
+
+    if leaking_func, do: raise exception_message(leaking_func)
+  end
+
+  defp context_lets(context) do
+    context
+    |> ESpec.Example.extract(ESpec.Let)
+    |> Enum.map(&(&1.var))
+  end
+
+  def all_functions(escaped_block) do
+    funs = ESpec.Let.AstParser.function_list(escaped_block)
+    if Enum.member?(funs, "should/1") || Enum.member?(funs, "is_expected/0") do
+      funs = ["subject/0" | funs]
     end
+    funs
+  end
 
-
-    leaking_func = Enum.find(diff, &(Enum.member?(funcs, &1)))
-
-    case leaking_func do
-      "subject/0" ->
-        raise "The subject is not defined in the current scope!"
-      string when is_binary(string) ->
-        raise "The let function `#{leaking_func}` is not defined in the current scope!"
-      nil -> :ok
+  defp exception_message(leaking_func) do
+    if leaking_func == "subject/0" do
+      "The subject is not defined in the current scope!"
+    else
+      "The let function `#{leaking_func}` is not defined in the current scope!"
     end
   end
 end
--- spec/some_spec.exs
+++ spec/some_spec.exs
@@ -1,15 +1,4 @@
 defmodule SomeSpec do
   use ESpec
-
-  let :a, do: 1
-  subject 2
-
-  it do
-    a |> should(eq 1)
-    should(a, eq 1)
-    a |> should(eq([1] |> List.first))
-    # should eq(2)
-  end
-
-
+  
 end
