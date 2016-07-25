ec5c3fbd9a7e6ee2c49f3b0d2412de74781f4e63
Anton Mishchuk <anton.mishchuk@gmail.com>
2016-07-26 00:28:23 +0300

    Remove skip-examples from test

--- test/mix/espec_init_test.exs
+++ test/mix/espec_init_test.exs
@@ -5,15 +5,10 @@ defmodule EspecInitTest do
 
   def clear, do: File.rm_rf! @tmp_path
 
-  setup context do
+  setup do
     Mix.shell(Mix.Shell.Process) # Get Mix output sent to the current process to avoid polluting tests.
-    function = if context[:skip_examples] do
-      fn -> Mix.Tasks.Espec.Init.run(["--skip-examples"]) end
-    else
-      fn -> Mix.Tasks.Espec.Init.run([]) end
-    end
     File.mkdir_p! @tmp_path
-    File.cd! @tmp_path, function
+    File.cd! @tmp_path, fn -> Mix.Tasks.Espec.Init.run([]) end
     on_exit(&clear/0)
     :ok
   end
