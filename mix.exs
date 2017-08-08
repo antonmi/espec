50016c786c70ac4e8f2620e4f6255f6e5cbb060d
Anton Mishchuk <anton.mishchuk@gmail.com>
2017-08-08 12:16:05 +0400

    Update deps versions

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule ESpec.Mixfile do
 
   defp deps do
     [
-      {:meck, "0.8.6"},
-      {:credo, "0.8.4", only: [:dev, :test]},
+      {:meck, "0.8.7"},
+      {:credo, "0.8.5", only: [:dev, :test]},
       # Docs
       {:ex_doc, "0.16.2", only: [:docs, :dev]}
     ]
