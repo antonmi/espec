8c3876914749f283aaccf607487c6b43689622d2
Bruce <brucepark0@gmail.com>
2017-11-04 17:41:33 -0700

    Add DateTime comparison logic for Assertions.Be

--- lib/espec/assertions/be.ex
+++ lib/espec/assertions/be.ex
@@ -6,15 +6,36 @@ defmodule ESpec.Assertions.Be do
   """
   use ESpec.Assertions.Interface
 
+  alias ESpec.DatesTimes.Comparator
+
+  defp match(subject, [op, val, {granularity, delta}]) do
+    actual_delta =
+      subject
+      |> Comparator.diff(val, granularity)
+      |> abs()
+
+    result = apply(Kernel, op, [subject, val]) && apply(Kernel, op, [actual_delta, delta])
+    {result, {granularity, actual_delta}}
+  end
+
   defp match(subject, [op, val]) do
     result = apply(Kernel, op, [subject, val])
     {result, result}
+  end
+
+  defp success_message(subject, [op, val, {granularity, delta}], _result, positive) do
+    to = if positive, do: "is true", else: "is false"
+    "`#{inspect subject} #{op} #{inspect val}` #{to} with delta `{:#{granularity}, #{delta}}`."
   end
 
   defp success_message(subject, [op, val], _result, positive) do
     to = if positive, do: "is true", else: "is false"
     "`#{inspect subject} #{op} #{inspect val}` #{to}."
   end  
+
+  defp error_message(subject, [op, val, {granularity, delta}], {actual_granularity, actual_delta}, positive) do
+    "Expected `#{inspect subject} #{op} #{inspect val}` to be `#{positive}` but got `#{!positive}` with delta `{:#{granularity}, #{delta}}`. The actual delta is {:#{actual_granularity}, #{actual_delta}}, with an inclusive boundary."
+  end
 
   defp error_message(subject, [op, val], _result, positive) do
     "Expected `#{inspect subject} #{op} #{inspect val}` to be `#{positive}` but got `#{!positive}`."
