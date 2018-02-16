cb0a92df5757394927aab8d7944097e6167cfa5a
Sascha Wolf <swolf.dev@gmail.com>
2018-02-16 18:12:33 +0100

    Spec - CheckErrorSharedSpec: Allow to check for extras

--- spec/shared/check_error_shared_spec.exs
+++ spec/shared/check_error_shared_spec.exs
@@ -5,7 +5,15 @@ defmodule CheckErrorSharedSpec do
     try do
       shared[:expectation].()
     rescue
-      error in [ESpec.AssertionError] -> expect(error.message) |> to(eq shared[:message])
+      error in [ESpec.AssertionError] ->
+        expect(error.message) |> to(eq shared[:message])
+
+        case shared[:extra] do
+          nil -> :ok
+          true -> expect(error.extra) |> to_not(be_nil())
+          false -> expect(error.extra) |> to(be_nil())
+          value -> expect(error.extra) |> to(eq value)
+        end
     end
   end
 end
