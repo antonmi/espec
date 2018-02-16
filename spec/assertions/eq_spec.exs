91b5bee4c840ccc6d710b99b1fe2155cc0875abc
Sascha Wolf <swolf.dev@gmail.com>
2018-02-16 18:13:37 +0100

    Spec - Eq: Check for `extra` in the eq specs

--- spec/assertions/eq_spec.exs
+++ spec/assertions/eq_spec.exs
@@ -19,7 +19,8 @@ defmodule ESpec.Assertions.EqSpec do
         before do
           {:shared,
            expectation: fn -> expect(1 + 1).to(eq(3.0)) end,
-           message: "Expected `2` to equal (==) `3.0`, but it doesn't."}
+           message: "Expected `2` to equal (==) `3.0`, but it doesn't.",
+           extra: true}
         end
 
         it_behaves_like(CheckErrorSharedSpec)
@@ -30,7 +31,8 @@ defmodule ESpec.Assertions.EqSpec do
           {:shared,
            expectation: fn -> expect(%{a: 2, b: 3, c: 4}).to(eq(%{a: 2, b: 4})) end,
            message:
-             "Expected `%{a: 2, b: 3, c: 4}` to equal (==) `%{a: 2, b: 4}`, but it doesn't."}
+             "Expected `%{a: 2, b: 3, c: 4}` to equal (==) `%{a: 2, b: 4}`, but it doesn't.",
+           extra: true}
         end
 
         it_behaves_like(CheckErrorSharedSpec)
@@ -40,7 +42,8 @@ defmodule ESpec.Assertions.EqSpec do
         before do
           {:shared,
            expectation: fn -> expect(1 + 1).to_not(eq(2)) end,
-           message: "Expected `2` not to equal (==) `2`, but it does."}
+           message: "Expected `2` not to equal (==) `2`, but it does.",
+           extra: false}
         end
 
         it_behaves_like(CheckErrorSharedSpec)
@@ -72,7 +75,8 @@ defmodule ESpec.Assertions.EqSpec do
         before do
           {:shared,
            expectation: fn -> expect(2 + 2).to(be 5) end,
-           message: "Expected `4` to equal (==) `5`, but it doesn't."}
+           message: "Expected `4` to equal (==) `5`, but it doesn't.",
+           extra: true}
         end
 
         it_behaves_like(CheckErrorSharedSpec)
@@ -82,7 +86,8 @@ defmodule ESpec.Assertions.EqSpec do
         before do
           {:shared,
            expectation: fn -> expect(1 + 1 == 1).to_not(be false) end,
-           message: "Expected `false` not to equal (==) `false`, but it does."}
+           message: "Expected `false` not to equal (==) `false`, but it does.",
+           extra: false}
         end
 
         it_behaves_like(CheckErrorSharedSpec)
