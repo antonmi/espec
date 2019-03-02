366556d307cee94ad0b3913debc04c3488657fb1
Anton Mishchuk <anton.mishchuk@gmail.com>
2019-03-02 13:21:05 +0200

    Fix Elixir 1.8 warnings

--- lib/espec/dates_times/types.ex
+++ lib/espec/dates_times/types.ex
@@ -1,6 +1,6 @@
 defmodule ESpec.DatesTimes.Types do
   # Complex types
   @type time_units ::
-          :microseconds | :milliseconds | :seconds | :minutes | :hours | :days | :weeks | :years
+          :microsecond | :millisecond | :second | :minute | :hour | :day | :week | :year
   @type calendar_types :: Date.t() | DateTime.t() | NaiveDateTime.t() | Time.t()
 end
