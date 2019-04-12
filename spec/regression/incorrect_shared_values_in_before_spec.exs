058351f932a79b41bd63839addf1be859d25017a
Sascha Wolf <swolf.dev@gmail.com>
2019-04-12 19:15:00 +0200

    Spec - Regression: Add a failing spec for incorrect shared values in before

---
+++ spec/regression/incorrect_shared_values_in_before_spec.exs
@@ -0,0 +1,13 @@
+defmodule Regression.IncorrectSharedValuesInBeforeSpec do
+  use ESpec, option: true
+
+  before do: send self(), {:option, shared[:option]}
+
+  it "option should be true" do
+    assert_receive {:option, true}
+  end
+
+  it "option should be false", option: false do
+    assert_receive {:option, false}
+  end
+end
