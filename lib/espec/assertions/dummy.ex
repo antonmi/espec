8c35888ef14e49a2c900b8065de62e581ef0fc39
Bruce <brucepark0@gmail.com>
2019-09-17 22:06:01 -0700

    Add Dummy spec to test changed modules
    When changing the dummy.ex file and running the stale feature, the
    associated dummy_spec.exs should be run again

---
+++ lib/espec/assertions/dummy.ex
@@ -0,0 +1,8 @@
+defmodule ESpec.Assertions.Dummy do
+  @moduledoc """
+  Defines 'dummy' assertion to test stale feature
+
+  """
+
+  def hello_world, do: "hello world"
+end
---
+++ spec/assertions/dummy_spec.exs
@@ -0,0 +1,48 @@
+defmodule ESpec.Assertions.DummySpec do
+  use ESpec, async: true
+
+  alias ESpec.Assertions.Dummy
+
+  describe "ESpec.Assertions.Be.Dummy" do
+    context "Success" do
+      it "checks success with `to`" do
+        message = expect(2) |> to(be(:>, 1))
+        expect(message) |> to(eq("`2 > 1` is true."))
+        expect(Dummy.hello_world()) |> to(eq("hello world"))
+      end
+
+      it "checks success with `not_to`" do
+        message = expect(2) |> to_not(be(:>, 3))
+        expect(message) |> to(eq("`2 > 3` is false."))
+      end
+
+      it(do: expect(1) |> to(be(:!=, 2)))
+      it(do: expect(1) |> to_not(be(:!=, 1)))
+
+      it(do: expect(1) |> to(be(:<=, 1)))
+      it(do: expect("abcd") |> to(be(:=~, ~r/c(d)/)))
+    end
+
+    context "Errors" do
+      context "with `to`" do
+        before do
+          {:shared,
+           expectation: fn -> expect(2) |> to(be(:>, 3)) end,
+           message: "Expected `2 > 3` to be `true` but got `false`."}
+        end
+
+        it_behaves_like(CheckErrorSharedSpec)
+      end
+
+      context "with `not_to`" do
+        before do
+          {:shared,
+           expectation: fn -> expect(1) |> to_not(be(:==, 1.0)) end,
+           message: "Expected `1 == 1.0` to be `false` but got `true`."}
+        end
+
+        it_behaves_like(CheckErrorSharedSpec)
+      end
+    end
+  end
+end
